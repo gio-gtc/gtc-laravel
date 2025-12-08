1f40f86434f10f1a7c1945f973b8ce95386114dc
Gio <gio@gtc.co>
2025-12-08 11:28:33 -0800

    Added a drop down menu to create button for create items

--- resources/js/components/globals/navigation/nav-main/create-btn.tsx
+++ resources/js/components/globals/navigation/nav-main/create-btn.tsx
@@ -1,10 +1,16 @@
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarTrigger,
+} from '@/components/ui/menubar';
 import { useSidebar } from '@/components/ui/sidebar';
 import {
     Tooltip,
     TooltipContent,
     TooltipTrigger,
 } from '@/components/ui/tooltip';
-import { Button } from '@headlessui/react';
 import { Plus } from 'lucide-react';
 
 export const CreateBtn = () => {
@@ -14,19 +20,26 @@ export const CreateBtn = () => {
 
     return (
         <Tooltip>
-            <TooltipTrigger asChild>
-                <Button className="mt-6 flex max-w-[100px] cursor-pointer gap-1.5 rounded-4xl bg-input p-2 align-middle">
-                    <div className="flex items-center justify-center rounded-full bg-gtc-red">
-                        <Plus
-                            style={{
-                                width: 'calc(var(--spacing) * 4)',
-                                height: 'calc(var(--spacing) * 4)',
-                            }}
-                        />
-                    </div>
-                    <span className={`text-sm ${hiddenClasses}`}>Create</span>
-                </Button>
-            </TooltipTrigger>
+            <Menubar>
+                <MenubarMenu>
+                    <TooltipTrigger asChild>
+                        <MenubarTrigger className="mt-6 flex max-w-[100px] cursor-pointer gap-1.5 rounded-4xl bg-input p-2 align-middle">
+                            <div className="flex items-center justify-center rounded-full bg-gtc-red">
+                                <Plus
+                                    style={{
+                                        width: 'calc(var(--spacing) * 4)',
+                                        height: 'calc(var(--spacing) * 4)',
+                                    }}
+                                />
+                            </div>
+                            <span className={`text-sm ${hiddenClasses}`}>
+                                Create
+                            </span>
+                        </MenubarTrigger>
+                    </TooltipTrigger>
+                    <CreateDropdown />
+                </MenubarMenu>
+            </Menubar>
             <TooltipContent
                 side="right"
                 align="center"
@@ -34,5 +47,15 @@ export const CreateBtn = () => {
                 children={<span>Create</span>}
             />
         </Tooltip>
+    );
+};
+
+const CreateDropdown = () => {
+    return (
+        <MenubarContent>
+            <MenubarItem>Organization</MenubarItem>
+            <MenubarItem>Contact</MenubarItem>
+            <MenubarItem>Tour</MenubarItem>
+        </MenubarContent>
     );
 };
