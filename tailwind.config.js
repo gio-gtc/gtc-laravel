c26bb18987fef97fb23af33920e9c60cdf9081b9
Gio <gio@gtc.co>
2026-02-11 08:10:35 -0800

    Removed dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        // ... your content paths
+    ],
+    darkMode: false, // Set this to false to disable dark mode
+    theme: {
+        extend: {
+            // ...
+        },
+    },
+    plugins: [
+        // ...
+    ],
+};
