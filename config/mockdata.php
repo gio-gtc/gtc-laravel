41b233915dcdd40be575776fff80bbb74a2094c4
Gio <gio@gtc.co>
2026-04-08 13:38:45 -0700

    Rorginizing catalog data to seperate data for better readability - finish

--- config/mockdata.php
+++ config/mockdata.php
@@ -1,62 +1,22 @@
 <?php
 
-$catalogPath = __DIR__.'/mockdata/generated/catalog.json';
+$ordersPath = __DIR__.'/mockdata/generated/orders.json';
 $homepagePath = __DIR__.'/mockdata/generated/homepage.json';
 $usersPath = __DIR__.'/mockdata/generated/users.json';
 $toursPath = __DIR__.'/mockdata/generated/tours.json';
 $toursVenuesPath = __DIR__.'/mockdata/generated/toursVenues.json';
 $venueItemsPath = __DIR__.'/mockdata/generated/venueItems.json';
 $venuesPath = __DIR__.'/mockdata/generated/venues.json';
-
-if (! is_file($catalogPath)) {
-    throw new RuntimeException(
-        'Missing demo catalog at config/mockdata/generated/catalog.json.'
-    );
-}
-
-if (! is_file($homepagePath)) {
-    throw new RuntimeException(
-        'Missing demo data at config/mockdata/generated/homepage.json.'
-    );
-}
-
-if (! is_file($usersPath)) {
-    throw new RuntimeException(
-        'Missing demo data at config/mockdata/generated/users.json.'
-    );
-}
-
-if (! is_file($toursPath)) {
-    throw new RuntimeException(
-        'Missing demo data at config/mockdata/generated/tours.json.'
-    );
-}
-
-if (! is_file($toursVenuesPath)) {
-    throw new RuntimeException(
-        'Missing demo data at config/mockdata/generated/toursVenues.json.'
-    );
-}
-
-if (! is_file($venueItemsPath)) {
-    throw new RuntimeException(
-        'Missing demo data at config/mockdata/generated/venueItems.json.'
-    );
-}
-
-if (! is_file($venuesPath)) {
-    throw new RuntimeException(
-        'Missing demo data at config/mockdata/generated/venues.json.'
-    );
-}
+$countriesPath = __DIR__.'/mockdata/generated/countries.json';
+$companiesPath = __DIR__.'/mockdata/generated/companies.json';
+$invoicesPath = __DIR__.'/mockdata/generated/invoices.json';
 
 
-/** @var array<string, mixed> $catalog */
-$catalog = json_decode(file_get_contents($catalogPath), true, 512, JSON_THROW_ON_ERROR);
+/** @var array<string, mixed> $orders */
+$orders = json_decode(file_get_contents($ordersPath), true, 512, JSON_THROW_ON_ERROR);
 
 /** @var array<int, array<string, mixed>> $homepage */
 $homepage = json_decode(file_get_contents($homepagePath), true, 512, JSON_THROW_ON_ERROR);
-
 
 /** @var array<int, array<string, mixed>> $users */
 $users = json_decode(file_get_contents($usersPath), true, 512, JSON_THROW_ON_ERROR);
@@ -73,16 +33,28 @@ $venueItems = json_decode(file_get_contents($venueItemsPath), true, 512, JSON_TH
 /** @var array<int, array<string, mixed>> $venues */
 $venues = json_decode(file_get_contents($venuesPath), true, 512, JSON_THROW_ON_ERROR);
 
+/** @var array<int, array<string, mixed>> $countries */
+$countries = json_decode(file_get_contents($countriesPath), true, 512, JSON_THROW_ON_ERROR);
+
+/** @var array<int, array<string, mixed>> $companies */
+$companies = json_decode(file_get_contents($companiesPath), true, 512, JSON_THROW_ON_ERROR);
+
+/** @var array<int, array<string, mixed>> $invoices */
+$invoices = json_decode(file_get_contents($invoicesPath), true, 512, JSON_THROW_ON_ERROR);
+
 
 return array_merge(
     [
         'demos' => require __DIR__.'/mockdata/demos.php',
     ],
-    $catalog,
+    $orders,
     $homepage,
     $users,
     $tours,
     $toursVenues,
     $venueItems,
-    $venues
+    $venues,
+    $countries,
+    $companies,
+    $invoices
 );
