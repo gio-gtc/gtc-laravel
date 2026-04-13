dc6f5dbf648fe926cfe9cb6215dbd81a8f363629
Gio <gio@gtc.co>
2026-04-13 11:53:46 -0700

    Creating Variants for reusable input component

--- resources/js/components/ui/input.tsx
+++ resources/js/components/ui/input.tsx
@@ -2,18 +2,24 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
+interface InputProps extends React.ComponentProps<'input'> {
+    variant?: 'default' | 'orderSlideout' | 'invoiceSlideout'; // Define your allowed strings here
+}
+
+function Input({ className, variant = 'default', type, ...props }: InputProps) {
+    const baseStyles =
+        'flex w-full min-w-0 rounded-md border border-input bg-transparent px-2.5 py-1 shadow-xs transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-gray-400 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20';
+
+    const variants = {
+        default: 'md-gray-900-weight-400  h-[44px]',
+        orderSlideout: '',
+        invoiceSlideout: '',
+    };
     return (
         <input
             type={type}
             data-slot="input"
-            className={cn(
-                'md-gray-900-weight-400',
-                'flex h-[44px] w-full min-w-0 rounded-md border border-input bg-transparent px-2.5 py-1 shadow-xs transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-gray-400 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
-                'focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50',
-                'aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40',
-                className,
-            )}
+            className={cn(baseStyles, variants[variant], className)}
             {...props}
         />
     );
